815497c04dcef941cceca4ab993e29b34826c4f4
Ben <116693980+benshomelab@users.noreply.github.com>
2022-10-25 22:38:55 -0400

    Create speedTestLog.py

---
+++ speedTestLog/speedTestLog.py
@@ -0,0 +1,24 @@
+import speedtest
+from LogAnalyticsLogging import *
+import time
+
+# Logging config
+newYorkTz = pytz.timezone("America/New_York")
+
+def startSpeedtest():
+	s = speedtest.Speedtest()
+	s.get_best_server()
+	s.download()
+	s.upload()
+	s.results.share()
+	results_dict = s.results.dict()
+	dSpeed = results_dict['download'] / 1048576
+	uSpeed = results_dict['upload'] / 1048576
+	send_log(uSpeed, dSpeed)
+
+while True:
+	try:
+		startSpeedtest()
+	except:
+		print('Something went wrong with speedtest')
+	time.sleep(90) # 900 for 15 mins
